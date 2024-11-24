5f7283ba45a10c357936871c8e50610fa9c80d2c
Alex <alexander.christianson@outlook.com>
2024-11-25 00:41:59 +0100

    migration

--- Migrations/20241124230124_new.Designer.cs
+++ Migrations/20241124230124_new.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using StoryPromptAPI.Data;
 
@@ -11,9 +12,11 @@ using StoryPromptAPI.Data;
 namespace StoryPromptAPI.Migrations
 {
     [DbContext(typeof(StoryPromptContext))]
-    partial class StoryPromptContextModelSnapshot : ModelSnapshot
+    [Migration("20241124230124_new")]
+    partial class @new
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
